e4358075e1c1f32f85b54f12d8adde84c326f768
Jakob Jónasson <jakob.jonasson@gmail.com>
2026-02-14 21:44:01 +0000

    test improvements

--- tests/test_date_utils.py
+++ tests/test_date_utils.py
@@ -11,7 +11,7 @@ def is_same_date(date1: datetime, date2: datetime):
 class TestParseTimePeriod:
     """Test cases for parse_time_period function."""
 
-    def test_return_one_day_ago_for_1d_period(self):
+    def test_should_return_one_day_ago_for_1d_period(self):
         event_period = "1d"
         expected = datetime.now() - timedelta(days=1)
 
@@ -19,7 +19,7 @@ class TestParseTimePeriod:
 
         assert is_same_date(result, expected)
 
-    def test_return_seven_days_ago_for_7d_period(self):
+    def test_should_return_seven_days_ago_for_7d_period(self):
         event_period = "7d"
         expected = datetime.now() - timedelta(days=7)
 
@@ -27,7 +27,7 @@ class TestParseTimePeriod:
 
         assert is_same_date(result, expected)
 
-    def test_return_thirty_days_ago_for_30d_period(self):
+    def test_should_return_thirty_days_ago_for_30d_period(self):
         event_period = "30d"
         expected = datetime.now() - timedelta(days=30)
 
@@ -35,7 +35,7 @@ class TestParseTimePeriod:
 
         assert is_same_date(result, expected)
 
-    def test_return_ninety_days_ago_for_90d_period(self):
+    def test_should_return_ninety_days_ago_for_90d_period(self):
         event_period = "90d"
         expected = datetime.now() - timedelta(days=90)
 
@@ -43,7 +43,7 @@ class TestParseTimePeriod:
 
         assert is_same_date(result, expected)
 
-    def test_return_thirty_days_ago_for_invalid_period(self):
+    def test_should_return_thirty_days_ago_for_invalid_period(self):
         event_period = "invalid"
         expected = datetime.now() - timedelta(days=30)
 
