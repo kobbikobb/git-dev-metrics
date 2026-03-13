b354a3b73972d8ab1f27a8064e6b80e168298484
Jakob Jonasson <jakob.jonasson@gmail.com>
2026-03-13 20:11:05 +0000

    Follow CompositePrinter pattern for stale PRs

--- git_dev_metrics/metrics/printer/__init__.py
+++ git_dev_metrics/metrics/printer/__init__.py
@@ -1,5 +1,6 @@
 from .base import Printer
 from .printers import CompositePrinter, ConsolePrinter, FilePrinter
+from .stale_printer import ConsoleStalePRPrinter, FileStalePRPrinter
 from .utils import get_default_output_path
 
 __all__ = [
@@ -7,5 +8,7 @@ __all__ = [
     "ConsolePrinter",
     "FilePrinter",
     "CompositePrinter",
+    "ConsoleStalePRPrinter",
+    "FileStalePRPrinter",
     "get_default_output_path",
 ]
